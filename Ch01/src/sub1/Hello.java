5365624bb6cbebbeae0c223da559c8613c857416
yun-setenforce <extendsabstractclass@gmail.com>
2023-06-13 15:21:53 +0900

    Ch01 수업예제

---
+++ Ch01/src/sub1/Hello.java
@@ -0,0 +1,27 @@
+package sub1;
+
+/**
+ * 날짜 : 2023/06/12
+ * 이름 : 정채윤
+ * 내용 : Ch01.Java 개발환경 구축
+ */
+public class Hello {
+	//프로그램 진입점 
+	public static void main(String[] args) {
+		
+		// 기본출력
+		
+		//window에서 ctrl+spacebar인 자동완성 기능 => shift+spacebar로 옮김 
+		System.out.println("Hello World~");
+		//sysout 자동완성 
+		System.out.println("Hello Java~");
+		
+		// 한줄출력 
+		System.out.print("Hello ");
+		System.out.print("Korea~");
+		
+		// 개행처리
+		System.out.print("\n");
+		System.out.print("Hello Busan~");
+	}
+}
