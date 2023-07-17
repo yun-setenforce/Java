2757d9f3912b87084fc621f1b4810a937051cf13
yun-setenforce <extendsabstractclass@gmail.com>
2023-07-17 10:52:48 +0900

    연습문제5

---
+++ Exam/src/test5/Test04.java
@@ -0,0 +1,25 @@
+package test5;
+/*
+ * 날짜 : 2023/07/17
+ * 이름 : 정채윤 
+ * 내용 : 자바 총정리 연습문제 
+ */
+public class Test04 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		
+		System.out.println(a > b && a == 10);
+		System.out.println(a > b && a == b);
+		
+		System.out.println(a > b || a == b );
+		System.out.println(a < b && a == b );
+
+		System.out.println(a > b ^ a == 10 );
+		System.out.println(a > b ^ a == b );
+		System.out.println(true ^ false );
+		
+		System.out.println(!(a > b));
+		System.out.println(!(a < b));
+	}
+}
