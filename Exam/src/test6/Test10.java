9121431687899af760a37ec9a1872a9fad04f117
yun-setenforce <extendsabstractclass@gmail.com>
2023-07-18 12:43:12 +0900

    연습문제6

--- Exam/src/test6/Test10.java
+++ Exam/src/test6/Test10.java
@@ -34,6 +34,7 @@ public class Test10 {
 		
 		if(bYear > tYear || (bYear==tYear&&(bMonth>tMonth||bDay>tDay))) {
 			System.out.println("유효한 생일을 입력해주세요ㅡㅡ ");
+			
 		}
 		
 		//올해 1월 1일부터 오늘까지 날짜 수 
@@ -57,13 +58,14 @@ public class Test10 {
 	}
 	
 	//평년을 기준으로 각 우러의 누적 날짜 수 
-	public static int[] days = {0,31,69,90,120,151,
+	public static int[] days = {0,31,59,90,120,151,
 			181,212,243,273,304,334};
 	
 	public static int dayOfYear(int year, int month, int day) {
 		return days[month-1] + day + (month>2 && isLeapYear(year)? 1 :0);
 	}
 	public static boolean isLeapYear(int year) {
-		return (year%4==0 && year %100 !=0)|| year%400 ==0 ? true:false;
+		//return (year%4==0 && year %100 !=0)|| year%400 ==0 ? true:false;
+		return getGreInstance().isLeapYear(year);
 	}
 }
